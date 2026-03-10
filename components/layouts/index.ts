7eafae7c35d2990a71f7ef3c6352658694cb92bb
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-03-10 09:13:34 -0500

    feat(help): add Help component with styling and internationalization support

--- components/layouts/index.ts
+++ components/layouts/index.ts
@@ -3,6 +3,7 @@ export * from './content'
 export * from './cover-title'
 export * from './footer'
 export * from './header'
+export * from './help'
 export * from './layout'
 export * from './page-not-found';
 export * from './page-title'
