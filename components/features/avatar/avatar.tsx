d54e710b1ea99145328ee495abc172801a076d91
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-03-13 17:31:07 -0500

    feat(avatar): enhance avatar component with titles and variation resolution logic

--- components/features/avatar/avatar.tsx
+++ components/features/avatar/avatar.tsx
@@ -1,27 +1,34 @@
+import * as React from 'react';
 import { Image } from '@ui';
 
 import { useOvaStore } from '@/store/ova-store';
 
-import { AVATARS } from './lib/constants';
+import { AVATAR_TITLE, AVATARS, resolveVariation } from './lib/constants';
 
 import type { AvatarVariation } from './types/type';
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
   variation: AvatarVariation;
-  alt: string;
   title?: string;
-  size: string;
-  hasHtml?: boolean;
+  size?: string;
   addClass?: string;
   noCaption?: boolean;
 }
 
-
-export const Avatar: React.FC<Props> = ({ variation, title = '', alt = '', size, hasHtml, noCaption, ...props }) => {
+export const Avatar: React.FC<Props> = ({
+  variation,
+  title = '',
+  size = '26.875rem',
+  noCaption,
+  ...props
+}) => {
   const selectedAvatarId = useOvaStore((state) => state.selectedAvatarId);
 
   const avatar = AVATARS.find(({ id }) => id === selectedAvatarId);
-  const src = avatar ? `assets/base/avatars/${avatar.id}/${avatar.name}-${variation}.webp` : '';
+  const resolvedVariation = avatar ? resolveVariation(avatar.variations, variation) : variation;
 
-  return <Image title={title} alt={alt} size={size} hasHtml={hasHtml} noCaption={noCaption} src={src} {...props} />;
+  const src = avatar ? `assets/base/avatars/${avatar.id}/${avatar.name}-${resolvedVariation}.webp` : '';
+  const alt = AVATAR_TITLE[variation] || 'Avatar.';
+
+  return <Image {...props} title={title} size={size} noCaption={noCaption} src={src} alt={alt} />;
 };
