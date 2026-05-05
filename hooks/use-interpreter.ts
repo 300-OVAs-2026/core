37ab370ba98770abfa36779b8684eb0039c04234
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-05-05 14:11:53 -0500

    fix(use-interpreter): refactor video source handling and optimize event dispatching

--- hooks/use-interpreter.ts
+++ hooks/use-interpreter.ts
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 import { eventChangeInterpreterVideo } from '@shared/utils';
 
@@ -30,9 +30,7 @@ export const useInterpreter = (): [(videoSource: VideoSourceChange) => void, () 
     'interpreter-video-source',
     INITIAL_VIDEO_STATE
   );
-  const lastVideoSources = useRef<VideoURLs>({ ...sources });
-
-  const BASE_PATH = import.meta.env.VITE_INTERPRETER_URL || 'assets/videos/interprete/';
+  const lastVideoSources = useRef<VideoURLs>({ ...INITIAL_VIDEO_STATE });
 
   /**
    * Actualiza las fuentes de video.
@@ -40,19 +38,18 @@ export const useInterpreter = (): [(videoSource: VideoSourceChange) => void, () 
    *
    * @param videoSource - Objeto que contiene las nuevas URLs de los videos y el modo de almacenamiento.
    */
-  const updateVideoSources = ({ mode, a11yURL, contentURL }: VideoSourceChange) => {
+  const updateVideoSources = useCallback(({ mode, a11yURL, contentURL }: VideoSourceChange) => {
     const { a11yURL: lastA11yURL, contentURL: lastContentURL } = lastVideoSources.current;
 
     // Si las nuevas URLs son iguales a las últimas, no hacer nada
-    if (lastA11yURL === `${BASE_PATH}${a11yURL}` && lastContentURL === `${BASE_PATH}${contentURL}`) return;
-    
+    if (lastA11yURL === a11yURL && lastContentURL === contentURL) return;
+
     // Actualizar las últimas fuentes de video
     lastVideoSources.current = { a11yURL, contentURL };
 
-    // Actualizamos las fuentes de los videos agregando la base.
     const updatedPaths = {
-      a11yURL: a11yURL ? `${BASE_PATH}${a11yURL}` : undefined,
-      contentURL: contentURL ? `${BASE_PATH}${contentURL}` : undefined
+      a11yURL,
+      contentURL
     };
 
     // Si el modo es 'fixed', actualizar las fuentes en el estado y el almacenamiento de sesión
@@ -63,15 +60,15 @@ export const useInterpreter = (): [(videoSource: VideoSourceChange) => void, () 
 
     // Enviar evento para notificar el cambio de fuentes de video
     eventChangeInterpreterVideo({ ...updatedPaths });
-  };
+  }, [setSources]);
 
   /**
    * Restaura las últimas fuentes de video guardadas en el estado.
    * Si las fuentes no son undefined, envía un evento para notificar el cambio.
    */
-  const restoreLastVideoSources = () => {
+  const restoreLastVideoSources = useCallback(() => {
     const updatedSource = getCurrentValueFromSessionStorage();
-    if (!Object.values(updatedSource).includes(undefined)) {
+    if (Object.values(updatedSource).some((url) => url !== undefined)) {
       const { a11yURL, contentURL } = updatedSource;
 
       // Actualizar las últimas fuentes de video
@@ -80,7 +77,7 @@ export const useInterpreter = (): [(videoSource: VideoSourceChange) => void, () 
       // Enviar evento para notificar el cambio de fuentes de video
       eventChangeInterpreterVideo({ a11yURL, contentURL });
     }
-  };
+  }, [getCurrentValueFromSessionStorage]);
 
   return [updateVideoSources, restoreLastVideoSources, sources];
 };
--- utils/event-change-interpreter-video.ts
+++ utils/event-change-interpreter-video.ts
@@ -2,6 +2,8 @@ import type { VideoURLs } from '@shared/hooks';
 
 import { EVENTS } from '../constants/events';
 
+const BASE_PATH = import.meta.env.VITE_INTERPRETER_URL || 'assets/videos/interprete/';
+
 /**
  * Función para despachar un evento personalizado para cambiar las fuentes de video del intérprete.
  * @param {Object} param0 - Objeto que contiene las nuevas URLs de accesibilidad y de contenido.
@@ -10,8 +12,12 @@ import { EVENTS } from '../constants/events';
  */
 export const eventChangeInterpreterVideo = ({ a11yURL, contentURL }: VideoURLs) => {
   // Crear un evento personalizado con los nuevos videos como detalle
+  // Si enviamos las URLs vacias el componente de interprete entenderá que debe ocultar el video correspondiente.
   const event = new CustomEvent(EVENTS.CHANGEINTERPRETEVIDEOSOURCES, {
-    detail: { accesibilityURL: a11yURL, contentURL }
+    detail: {
+      accesibilityURL: a11yURL ? `${BASE_PATH}${a11yURL}` : undefined,
+      contentURL: contentURL ? `${BASE_PATH}${contentURL}` : undefined
+    }
   });
 
   // Despachar el evento a nivel del documento
